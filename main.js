90fa3e40162ebb783cfbf8fec9fa932298cdf489
heejin <lalahabi12@gmail.com>
2024-02-12 09:08:57 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList=[];
+addButton.addEventListener("click", addTask);
+
+
+function addTask(){
+    let taskContent = taskInput.value;
+    taskList.push(taskContent)
+    console.log(taskList);
+    render();
+}
+
+function render(){
+  let resultHTML = "";
+  for(i=0; i<=taskList.length;i++){
+    resultHTML += `<div class ="task">
+    <div>
+     ${taskList[i]}
+    </div>
+    <div>
+      <button>Check</button>
+      <button>Delete</button>
+    </div>
+  </div>`
+  }
+
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
